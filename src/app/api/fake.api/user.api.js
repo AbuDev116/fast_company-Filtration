927047247048a4bdf7f3149cf295b7329116f06f
Abusev Farid <116abudev@gmail.com>
2022-12-07 17:48:01 +0300

    homework preparation. added new fake.api  1) need to add the ability to work with arrays 2) make an asynchronous request for users.jsx

--- src/app/api/fake.api/user.api.js
+++ src/app/api/fake.api/user.api.js
@@ -1,4 +1,4 @@
-import { professions } from "./professions.api";
+import { professionsObject as professions } from "./professions.api";
 const qualities = {
     tedious: {
         _id: "67rdca3eeb7f6fgeed471198",
@@ -143,9 +143,12 @@ const users = [
     }
 ];
 
-const fetchAll = () => {
-    return users;
-};
+const fetchAll = () =>
+    new Promise((resolve) => {
+        window.setTimeout(function () {
+            resolve(users);
+        }, 2000);
+    });
 
 export default {
     fetchAll
