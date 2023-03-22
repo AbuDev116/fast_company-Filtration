20cec4e0f68195072914895c4e2a3059fbd6cff7
Abusev Farid <116abudev@gmail.com>
2023-03-22 15:00:10 +0300

    added new folder and file structure

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Users from "./layouts/users";
-import NavBar from "./components/navbar";
-import { Route, Switch } from "react-router-dom";
+import NavBar from "./components/ui/navbar";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Main from "./layouts/main";
 import Login from "./layouts/login";
 
@@ -10,9 +10,10 @@ function App() {
         <>
             <NavBar />
             <Switch>
-                <Route path="/" exact component={Main} />
+                <Route path="/users/:userId?" component={Users} />
                 <Route path="/login" component={Login} />
-                <Route path="/users" component={Users} />
+                <Route path="/" exact component={Main} />
+                <Redirect to="/" />
             </Switch>
         </>
     );
